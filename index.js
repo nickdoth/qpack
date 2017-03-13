264af58d3698e70433b7730fca5cd21bbb2a7b18
Nick Ng <nick.doth@gmail.com>
2017-03-14 03:42:25 +0800

    parse external/require

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const browserify = require("browserify");
 
 function pack(input, output, opt) {
     var bundler = browserify(input, opt);
-        
+    opt.basedir = opt.basedir || process.cwd();
+    // Parse externals
+    if (opt.external) {
+        bundler.external(opt.external);
+    }
+    // Parse require
+    if (opt.require) {
+        opt.require.forEach(r => bundler.require.apply(bundler, r));
+    }
+    // By default using uglifyify when in production environment
     if (process.env.NODE_ENV === 'production') {
         console.log('production');
         bundler.transform('uglifyify', { global: true });
@@ -18,12 +27,12 @@ function pack(input, output, opt) {
 }
 
 pack.forHarmony = {
-    transform: {
-        'babelify': {
+    transform: [
+        ['babelify', {
             presets: ["env", "react"],
             plugins: ["transform-es2015-destructuring", "transform-object-rest-spread"]
-        }
-    }
+        }]
+    ]
 };
 
 pack.forNodejs = {
